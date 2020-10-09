1ca712ff318175ab384c74b2c533cd84cb65aab0
Lumeric <71871188+Lumeric@users.noreply.github.com>
2020-10-09 16:28:10 +0700

    Add files via upload

---
+++ testTree/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace testTree
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xmlTreeView = new System.Windows.Forms.TreeView();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.controlButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // xmlTreeView
+            // 
+            this.xmlTreeView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.xmlTreeView.Location = new System.Drawing.Point(12, 12);
+            this.xmlTreeView.MinimumSize = new System.Drawing.Size(300, 400);
+            this.xmlTreeView.Name = "xmlTreeView";
+            this.xmlTreeView.Size = new System.Drawing.Size(300, 400);
+            this.xmlTreeView.TabIndex = 0;
+            // 
+            // loadButton
+            // 
+            this.loadButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.loadButton.Location = new System.Drawing.Point(12, 418);
+            this.loadButton.MaximumSize = new System.Drawing.Size(125, 40);
+            this.loadButton.MinimumSize = new System.Drawing.Size(125, 40);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(125, 40);
+            this.loadButton.TabIndex = 1;
+            this.loadButton.Text = "Load XML File";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // controlButton
+            // 
+            this.controlButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.controlButton.Location = new System.Drawing.Point(188, 418);
+            this.controlButton.MaximumSize = new System.Drawing.Size(125, 40);
+            this.controlButton.MinimumSize = new System.Drawing.Size(125, 40);
+            this.controlButton.Name = "controlButton";
+            this.controlButton.Size = new System.Drawing.Size(125, 40);
+            this.controlButton.TabIndex = 2;
+            this.controlButton.Text = "Expand/Collapse";
+            this.controlButton.UseVisualStyleBackColor = true;
+            this.controlButton.Click += new System.EventHandler(this.controlButton_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(325, 463);
+            this.Controls.Add(this.controlButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.xmlTreeView);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.MinimumSize = new System.Drawing.Size(343, 510);
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "mainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView xmlTreeView;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button controlButton;
+    }
+}
+
---
+++ testTree/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Drawing.Text;
+
+namespace testTree
+{
+    public partial class mainForm : Form
+    {
+        private bool treeIsExpanded = false;
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openXmlFileDialog = new OpenFileDialog();
+            openXmlFileDialog.Filter = "XML Files (*.xml)|*.xml";
+            openXmlFileDialog.FilterIndex = 2;
+            openXmlFileDialog.RestoreDirectory = true;
+
+            if (openXmlFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                RenderXMLFile(openXmlFileDialog.FileName);
+            }
+        }
+
+        private void RenderXMLFile(string filepath)
+        {
+            try
+            {
+                string xmlString = File.ReadAllText(filepath, Encoding.UTF8);
+
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(xmlString);
+
+                xmlTreeView.Nodes.Clear();
+                xmlTreeView.Nodes.Add(new TreeNode(xmlDoc.DocumentElement.Name));
+
+                TreeNode treeNode = new TreeNode();
+                treeNode = xmlTreeView.Nodes[0];
+
+                AddNode(xmlDoc.DocumentElement, treeNode);
+            }
+            catch (XmlException xmlEx)
+            {
+                MessageBox.Show(xmlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void AddNode(XmlNode inXmlNode, TreeNode inTreeNode)
+        {
+            XmlNode xmlNode;
+            TreeNode treeNode;
+            XmlNodeList xmlNodeList;
+
+            if (inXmlNode.HasChildNodes)
+            {
+                xmlNodeList = inXmlNode.ChildNodes;
+
+                for (int i = 0; i <= xmlNodeList.Count - 1; i++)
+                {
+                    xmlNode = inXmlNode.ChildNodes[i];
+                    inTreeNode.Nodes.Add(new TreeNode(xmlNode.Name));
+                    treeNode = inTreeNode.Nodes[i];
+                    AddNode(xmlNode, treeNode);
+                }
+            }
+            else
+            {
+                inTreeNode.Text = inXmlNode.OuterXml.Trim();
+            }
+        }
+
+        private void controlButton_Click(object sender, EventArgs e)
+        {
+            if (treeIsExpanded)
+            {
+                xmlTreeView.CollapseAll();
+                treeIsExpanded = false;  
+            }
+            else
+            {
+                xmlTreeView.ExpandAll();
+                treeIsExpanded = true;
+            }
+        }
+    }
+}
+
